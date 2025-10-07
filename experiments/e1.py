6aec957fb7abc01877c04c38e3dc62364d9f4f86
thanh <ngdinhthanh91@gmail.com>
2025-10-07 22:01:31 +0700

    Remove bonus and experiments folders from git tracking

--- experiments/e1.py
+++
@@ -1,8 +0,0 @@
-import glob
-
-myfiles = glob.glob("files/*.csv")
-
-for filepath in myfiles:
-    with open(filepath, 'r') as file:
-        print(f"Contents of {filepath}:")
-        print(file.read())
