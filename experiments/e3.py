6bf812d0ba9af1b90339a5296cfaa7a63b61e0eb
thanh <ngdinhthanh91@gmail.com>
2025-10-07 22:12:44 +0700

    Remove bonus and experiments folders from git tracking for second time

--- experiments/e3.py
+++
@@ -1,5 +0,0 @@
-import webbrowser
-
-user_term = input("Enter a search term: ")
-url = f"https://www.google.com/search?q={user_term}"
-webbrowser.open(url)  # This will open the default web browser to the specified URL
