b807f9a66c20621b0aeb4693a331e3b3c31e2290
thanh <ngdinhthanh91@gmail.com>
2025-10-10 23:07:29 +0700

    Stop tracking files folder

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
 # define the help functions
-def get_todos(file_path="files/todos.txt"):
+def get_todos(file_path="todos.txt"):
     """Read a text file and return the list of to-do items."""
     with open(file_path, "r") as file:
         todos = file.readlines()
     return todos
 
-def write_todos(todos, file_path="files/todos.txt"):
+def write_todos(todos, file_path="todos.txt"):
     """Write the list of to-do items to a text file."""
     with open(file_path, "w") as file:
         file.writelines(todos)
